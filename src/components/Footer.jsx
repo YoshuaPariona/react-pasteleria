3367dcd28be7b2bee6cf727fa3fffe269a1f553e
YoshuaPariona <loganpariona.1207@gmail.com>
2025-05-22 18:28:40 -0500

    Add AboutPage component with basic structure and heading

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,34 @@
 export const Footer = () => {
-    return (
-        <h1>aver2</h1>
-    )
-}
+  return (
+    <footer className="relative w-full">
+        
+      <img
+        src="src/assets/footer.jpg"
+        alt="Fondo"
+        className="absolute inset-0 w-full h-full object-cover z-0"
+      />
+
+      <div className="absolute inset-0 bg-black opacity-50 z-0" />
+
+      <div className="relative z-10 text-white px-6 py-10 flex flex-col items-left">
+        <div className="flex flex-col md:flex-row justify-around gap-6 w-full max-w-4xl text-center md:text-left">
+          <div>
+            <h2 className="text-lg font-semibold mb-1">Contacto</h2>
+            <p>📞 987 654 321</p>
+            <p>📧 panaderiadboyos@gmail.com</p>
+          </div>
+          <div>
+            <h2 className="text-lg font-semibold mb-1">Redes</h2>
+            <p>Facebook · Instagram · WhatsApp</p>
+          </div>
+        </div>
+
+        <hr className="my-6 border-gray-600 w-full max-w-4xl" />
+
+        <p className="text-sm text-gray-400 px-65">
+          &copy; 2025 Panadería D'Boyos'. Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+};
