66c3e6a20e003f778c49871d1c1f767f626bc99d
YoshuaPariona <loganpariona.1207@gmail.com>
2025-05-22 18:28:40 -0500

    feat: Add AboutPage with basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { ProductsPage } from "./pages/shop/ProductsPage"
 import { HomePage } from "./pages/shop/HomePage"
 import { ShopLayout } from "./layout/ShopLayout"
+import { AboutPage } from "./pages/shop/AboutPage"
 
 function App() {
   
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<ShopLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/productos" element={<ProductsPage />} />
+          <Route path="/nosotros" element={<AboutPage />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/shop/AboutPage.jsx
@@ -0,0 +1,7 @@
+export const AboutPage = () => {
+    return (
+        <>
+            <h1>Pagina de nosotros</h1>
+        </>
+    )
+};
